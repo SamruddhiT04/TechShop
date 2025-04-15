26ac5a573c2b56b7f025989242a93758d96f5cb0
SamruddhiT04 <samruddhitekade01@gmail.com>
2025-04-15 11:35:21 +0530

    Delete TechShop directory

--- TechShop/src/exception/IncompleteOrderException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class IncompleteOrderException extends Exception {
-    public IncompleteOrderException(String message) {
-        super(message);
-    }
-}
--- TechShop/src/exception/PaymentFailedException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class PaymentFailedException extends Exception {
-    public PaymentFailedException(String message) {
-        super(message);
-    }
-}
